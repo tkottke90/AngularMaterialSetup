0bd653803160eb2ef4190505cdaa07734306b9e7
tkottke90 <altotom90@gmail.com>
2017-06-18 23:59:46 -0500

    Work 6/17-6/18
    - Added Project Display Element that will show over all main content to
    give more detail about the information the user clicks on.

---
+++ src/app/project-display/project-display.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-projectdisplay',
+    templateUrl : './project-display.component.html',
+    styleUrls : ['./project-display.component.css']
+})
+export class ProjectDisplay {
+
+}
---
+++ src/app/services/project-info.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class ProjectInfo{
+
+}
+
+export class Project {
+    
+    name: string;
+    description: string;
+
+}
