fabe0743b6bdc2cba7207c2f259188c4c96bd956
tkottke90 <altotom90@gmail.com>
2017-07-09 21:22:01 -0500

    Moved compoenent declaration to own file for cleaner organization

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,17 +13,17 @@ import 'hammerjs';
 import { Components } from './app-components'
 
 import { AppComponent } from './app.component';
-import { Component1Component } from './component1/component1.component';
-import { IntroComponentComponent } from './intro-component/intro-component.component';
-import { ProjectsComponent } from './projects/projects.component';
-import { ProjectCardComponent } from './project-card/project-card.component';
-import { ProDevComponent } from './pro-dev/pro-dev.component';
-import { DetailContainer } from './details-container/details-container.component';
-import { ProCardComponent } from './pro-card/pro-card.component';
-import { SkillComponent } from './skill/skill.component';
-import { SkillContainerComponent } from './skill-container/skill-container.component';
-import { ProjectDisplay } from './project-display/project-display.component';
-import { ProjectDetails } from './project-display/project-details/project-details.component'
+// import { Component1Component } from './component1/component1.component';
+// import { IntroComponentComponent } from './intro-component/intro-component.component';
+// import { ProjectsComponent } from './projects/projects.component';
+// import { ProjectCardComponent } from './project-card/project-card.component';
+// import { ProDevComponent } from './pro-dev/pro-dev.component';
+// import { DetailContainer } from './details-container/details-container.component';
+// import { ProCardComponent } from './pro-card/pro-card.component';
+// import { SkillComponent } from './skill/skill.component';
+// import { SkillContainerComponent } from './skill-container/skill-container.component';
+// import { ProjectDisplay } from './project-display/project-display.component';
+// import { ProjectDetails } from './project-display/project-details/project-details.component'
 
 
 import { routing } from './app.routing';
@@ -37,7 +37,7 @@ import { UsageLog } from './services/site-log.service';
 @NgModule({
   declarations: [
     AppComponent,
-    Component1Component,
+    // Component1Component,
     Components
     // IntroComponentComponent,
     // ProjectsComponent,
